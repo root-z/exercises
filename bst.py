dca4d30d82e009d54579f37cdcc755d059373b54
Zhiyuan Lin <z72lin@uwaterloo.ca>
2016-09-14 22:40:25 -0400

    finish

--- bst.py
+++ bst.py
@@ -21,6 +21,15 @@ class BSTree:
         else:
             self.insertNode(self.root, node)
 
+    def print_tree(self, indent=0):
+        BSTree.print_subtree(self.root)
+
+    def print_subtree(node: BSTreeNode, indent=0):
+        if node is not None:
+            print(indent*' ' + str(node.value))
+            BSTree.print_subtree(node.left, indent+2)
+            BSTree.print_subtree(node.right, indent+2)
+            
     def insertNum(self, num):
         self.insert(BSTreeNode(num))
             
@@ -43,7 +52,7 @@ class BSTree:
         while True:
             if curr.value == num:
                 return curr
-            else if curr.value < num:
+            elif curr.value < num:
                 if curr.right is None:
                     raise Error('not found')
                 else:
@@ -57,24 +66,25 @@ class BSTree:
     def delete(self, node):
         if self.root == node:
             self.root = None
-        
+            return
+            
         curr = self.root
         previous = None
         
         while True:
             if curr < node:
+                previous = curr
                 curr = curr.right
-                previous = curr
                 left = False
-            else if curr > node:
-                curr = curr.left
+            elif curr > node:
                 previous = curr
+                curr = curr.left
                 left = True
             else:
                 if curr.left is None:
                     curr = curr.right
-                else if curr.right is None:
-                    curr = curr.right
+                elif curr.right is None:
+                    curr = curr.left
                 else: # have both left and right
                     last_left, last_previous = find_last_left(curr.right)
                     if last_left == curr.right:
@@ -85,11 +95,14 @@ class BSTree:
                         last_left.right = curr.right
                         last_left.left = curr.left
                         curr = last_left
-                                    
-                if left:
-                    previous.left = curr
-                else:
-                    previous.right = curr
+                break
+                    
+        if left:
+            previous.left = curr
+        else:
+            previous.right = curr
+                
+            
 
 def find_last_left(node):
     curr = node
@@ -104,4 +117,8 @@ def find_last_left(node):
 t = BSTree()
 t.insertNum(1)
 t.insertNum(2)
-                
+t.print_tree()
+node = t.find(2)
+print(node.value)
+t.delete(node)
+t.print_tree()
