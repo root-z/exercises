e64efbd43ea5ae9e9879cacb532e4a58ebbd1e56
Zhiyuan Lin <z72lin@uwaterloo.ca>
2016-09-21 22:49:13 -0400

    heap sort

--- heap.py
+++ heap.py
@@ -1,9 +1,15 @@
 from math import floor
+import copy
 
-class heap:
+class Heap:
     def __init__(self, elems = []):
-        self.__elems = elems
-
+        self.__elems = copy.copy(elems)
+        for i in range(self.heap_size()//2 - 1, -1, -1):
+            self.max_heapify(i)
+            
+    def __str__(self):
+        return str(self.__elems)
+            
     def get(self, index):
         '''
         get element from array
@@ -22,13 +28,19 @@ class heap:
         '''
         return self.__elems[floor(index)]
 
-    def left(self, index):
+    def left_index(self, index):
+        return 2*index
+    
+    def left_child(self, index):
         '''
         left child
         '''
         return self.__elems[2*index]
 
-    def right(self, index):
+    def right_index(self, index):
+        return 2*index + 1
+    
+    def right_child(self, index):
         '''
         right child
         '''
@@ -44,13 +56,13 @@ class heap:
         '''
         maintaining the heap property
         '''
-        l = left(i)
-        r = right(i)
-        if l <= self.heap_size() and self.get(l) > self.get(i):
+        l = self.left_index(i)
+        r = self.right_index(i)
+        if l < self.heap_size() and self.get(l) > self.get(i):
             largest = l
         else:
             largest = i
-        if r <= self.heap_size() and self.get(r) > self.get(largest):
+        if r < self.heap_size() and self.get(r) > self.get(largest):
             largest = r
         if largest != i:
             tmp = self.get(i)
@@ -58,6 +70,10 @@ class heap:
             self.set(largest, tmp)
             self.max_heapify(largest)
 
+if __name__=='__main__':
+    h = Heap([5,3,2,4,1])
+    print(h)
+    
 
             
         
