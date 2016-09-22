1d0bb7e8b4573548f9db2b789abd590fd524a94f
Zhiyuan Lin <z72lin@uwaterloo.ca>
2016-09-21 22:28:41 -0400

    max_heapify

--- heap.py
+++ heap.py
@@ -4,14 +4,60 @@ class heap:
     def __init__(self, elems = []):
         self.__elems = elems
 
-    def get(index):
+    def get(self, index):
+        '''
+        get element from array
+        '''
         return self.__elems[index]
 
-    def parent(index):
+    def set(self, index, value):
+        '''
+        set index
+        '''
+        self.__elems[index] = value
+        
+    def parent(self, index):
+        '''
+        get parent node
+        '''
         return self.__elems[floor(index)]
 
-    def left(index):
+    def left(self, index):
+        '''
+        left child
+        '''
         return self.__elems[2*index]
 
-    def right(index):
+    def right(self, index):
+        '''
+        right child
+        '''
         return self.__elems[2*index+1]
+
+    def heap_size(self):
+        '''
+        size of the heap
+        '''
+        return len(self.__elems)
+    
+    def max_heapify(self, i):
+        '''
+        maintaining the heap property
+        '''
+        l = left(i)
+        r = right(i)
+        if l <= self.heap_size() and self.get(l) > self.get(i):
+            largest = l
+        else:
+            largest = i
+        if r <= self.heap_size() and self.get(r) > self.get(largest):
+            largest = r
+        if largest != i:
+            tmp = self.get(i)
+            self.set(i, self.get(largest))
+            self.set(largest, tmp)
+            self.max_heapify(largest)
+
+
+            
+        
